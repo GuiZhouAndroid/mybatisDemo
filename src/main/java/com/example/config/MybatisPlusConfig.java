6d57878f8f8be17b56ab913cf3e3903223af9f8c
GuiZhouAndroid <2393579405@qqcom>
2021-10-01 16:15:47 +0800

    待使用mybatis plus 3.5.1，清空旧版资源

--- src/main/java/com/example/config/MybatisPlusConfig.java
+++ src/main/java/com/example/config/MybatisPlusConfig.java
@@ -10,6 +10,7 @@ package com.example.config;
 
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
@@ -39,6 +40,9 @@ public class MybatisPlusConfig {
 //    }
 
     // 最新版
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
